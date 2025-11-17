aa76c94639e67f6e40deae72fa21d6b3452ebd4e
karalarmehmet <mehmetkaralarm@gmail.com>
2025-11-17 23:18:57 +0300

    fibonacci

--- problems/fibonacci3.go
+++ problems/fibonacci3.go
@@ -1,39 +1,41 @@
 package main
 
-import "fmt"
-
-// Iterative approach using a slice
-func fibonacciIterative1(n int) int {
-	if n <= 1 {
-		return n
-	}
-	// Create a slice to store Fibonacci numbers
-	fib := make([]int, n+1)
-	fib[0], fib[1] = 0, 1
-
-	for i := 2; i <= n; i++ {
-		fib[i] = fib[i-1] + fib[i-2]
-	}
-
-	return fib[n]
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var num int
-	fmt.Println("Enter the number of Fibonacci terms")
+	fmt.Println("Enter fibonacci number that you want to calculate")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	number, err := strconv.Atoi(input)
 
-	for {
-		a, err := fmt.Scan(&num) // a is the number of items that is scanned succesfully we dont have anything to do with that
-		fmt.Println(a)
-
-		if err != nil || num < 0 {
-			fmt.Println("please enter a valid input")
-			continue
-		}
-		break
+	if number < 0 || err != nil {
+		fmt.Println("Enter a valid number")
 	}
 
-	// Compute the nth Fibonacci number
-	result := fibonacciIterative1(num)
-	fmt.Printf("The %dth Fibonacci number is %d\n", num, result)
+	fmt.Println(fibonacci(number))
 }
+
+func fibonacci(a int) int {
+	if a <= 1 {
+		return a
+	}
+	prev := 0
+	curr := 1
+
+	for i := 2; i <= a; i++ {
+		prev, curr = curr, curr+prev
+
+	}
+	return curr
+}
+
+// When you call reader.ReadString('\n'), Go reads everything up until the newline and stores it in the input variable.
+// The internal buffer that bufio.NewReader uses is automatically managed. After ReadString reads the input, it clears the buffer for the next read operation. So, the buffer is cleared after each call to ReadString, whether the input is valid or not.
+// If you enter -4, the input "-4\n" is read into the buffer and stored in input. The buffer is then cleared, and the next time the program asks for input, it is ready to take new data without any leftover characters.
